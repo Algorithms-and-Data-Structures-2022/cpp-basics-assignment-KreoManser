0861b8515e6824fc041dd62d24740d6dddd277c3
kreomanser <sherba.ru@icloud.com>
2022-03-03 18:47:34 +0300

    Test push 1

--- src/code.cpp
+++ src/code.cpp
@@ -7,46 +7,53 @@ namespace assignment {
 
   // Task 1
   int squared_diff(int left, int right) {
-
-    // Write your code here ...
-
-    return 0;
+    return ((left - right)*(left - right));
   }
 
   // Task 2
   bool check_bit(int mask, int bit_pos) {
-
-    // Write your code here ...
-
+    if(mask & (1 << bit_pos) && mask >= 0 && bit_pos >= 0)
+      return true;
     return false;
   }
 
   // Task 3
   int max3(int left, int middle, int right) {
-
-    // Write your code here ...
-
-    return 0;
+    int max = 0;
+    if (left > middle && left > right) {max = left;}
+    if (middle > left && middle > right){max = middle;}
+    if (right > middle && right > left) {max = right;}
+    return max;
   }
 
   // Task 4
-  void swap_args(int *left, int *right) {
-
-    // Write your code here ...
+  int *swap_args(int *left, int *right) {
+    if (left == nullptr || right == nullptr){
+      return (left, right);
+    } else {
+      int temp_left = *left;
+      *left = *right;
+      *right = temp_left;
+    }
   }
 
   // Task 5
   int arr_sum(int *arr, int length) {
-
-    // Write your code here ...
-
+    int Summa = 0;
+    for(int i = 0; i < length; i++) {
+      Summa += arr[i];
+    }
     return 0;
   }
 
   // Task 6
   int *find_max_elem(int *arr, int length) {
-
-    // Write your code here ...
+    int Max = arr[0];
+    for (int i = 0; i < length; i++) {
+      if (Max < arr[i]) {
+        Max = arr[i];
+      }
+    }
 
     return nullptr;
   }
@@ -54,15 +61,16 @@ namespace assignment {
   // Task 7
   int *allocate_arr(int length, int init_value) {
 
-    // Write your code here ...
 
     return nullptr;
   }
 
   // Task 8
   int *clone_arr(int *arr_in, int length) {
-
-    // Write your code here ...
+    int *arr_out = new int[length];
+    for(int i = 0; i < length; i++){
+      arr_out[i] = arr_in[i];
+    }
 
     return nullptr;
   }
